04e959e8100df34db13555ebc912b4927750aed7
franco <f4072421@gmail.com>
2026-06-20 17:40:17 -0300

    Update data.js

--- data.js
+++ data.js
@@ -19,5 +19,11 @@ const tareasLiceo = [
         tipo: "Tarea",
         fecha: "2026-06-26",
         descripcion: "Completar la guía de ejercicios prácticos del 1 al 10."
+    }    
+    {
+        materia: "Biologia",
+        tipo: "Tarea",
+        fecha: "2026-06-26",
+        descripcion: "No. Soy un hombre heterosexual perfectamente sano que no quiere que esos temas de MMO/deep rpg mechics se metan con mi juego de acción/aventura en tercera persona. Actúas como si tuvieras algún tipo de superioridad moral sobre mí, ¿pero por qué?? ¿Porque me gustan las mujeres? ¿Porque soy capaz de hacer la distinción entre fantasía y realidad? No, necesitas mirarte al espejo cuando se trata de estos temas. ¿Cómo te comportas en persona? En mi vida pública/social, soy extremadamente reservado. Tengo un respeto profundo por las mujeres que no se andan poniendo de acuerdo con esas normas sociales modernas de enseñar todo su cuerpo por fama y dinero en línea o en persona. Tengo una gran admiración por la cultura japonesa y por su increíble habilidad para mantener la vida pública y la privada completamente separadas. No sabes nada de mí. Y aun así te sientas aquí y me avergüenzas por gustar de la figura femenina en la fantasía? ¿Pero qué hay de las otras cosas de este juego? ¿Eres tan intolerante con la atracción de un hombre hacia la figura femenina, pero aún así, te parece completamente bien el narcotráfico, el asesinato, niveles impresionantes de consumismo y niveles enormes de corrupción? ¿Dónde está tu base moral para pararte ahora? ¿Qué es lo peor? ¿Un personaje hermoso (de ficción) con una mini falda o un personaje corriendo por ahí matando gente por poder y riqueza? La gente como tú me da asco. No tienes ningún pensamiento crítico. Estás parado con firmeza sobre un terreno tan tambaleante. Incapaz de separarte de la fantasía. Todo lo que ves se convierte en un ataque interpersonal contra ti. Todo siempre va de ti. Tú eres el verdadero sexista."
     }
 ];
